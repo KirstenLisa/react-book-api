956f3cc4c6c58adb5de3591237f4453add55d95f
Kirsten Kustusch <kirsten.kustusch@gmail.com>
2019-10-27 16:41:41 +0200

    added Info function

--- src/ListItem/ListItem.js
+++ src/ListItem/ListItem.js
@@ -7,23 +7,32 @@ class ListItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showAddInfo: false
+      showInfo: false
     };
   }
 
+  setShowInfo(show) {
+    this.setState({
+      showInfo: show
+    });
+  }
+
+
   render() {
 
-    
-
+    const description = this.state.showInfo ? 
+                        <div className="ListItem__description">{this.props.volumeInfo.description}</div>
+                        : <button className="infoButton" onClick={show => this.setShowInfo(show)}>More</button>;
     return (
       <div className="ListItem">
-          <div className="BookCover"> <img src={'this.props.volumeInfo.imageLinks.thumbnail'} alt="Book Cover" /></div> 
-            <div className="ListItem__heading">
-              <div className="ListItem__title">{this.props.volumeInfo.title}</div>
-              <div className="ListItem__author">{this.props.volumeInfo.authors}</div>
+          <div className="BookCover"> <img src={this.props.volumeInfo.imageLinks.thumbnail} alt="Book Cover" /></div> 
+          <div className="ListItem__heading">
+            <div className="ListItem__title">{this.props.volumeInfo.title}</div>
+            <div className="ListItem__author">Author: {this.props.volumeInfo.authors}</div>
+            <div className="ListItem__Price">Price: {this.props.saleInfo.saleability === "FOR_SALE" ? this.props.saleInfo.listPrice.amount : 'not for sale'}</div>
           </div>
           <div className="ListItem__addinfo">
-            <div className="ListItem__description">{this.props.volumeInfo.description}</div>
+            { description }
            
           </div>
             
